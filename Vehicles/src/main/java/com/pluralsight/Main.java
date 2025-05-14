71a44c38d761a48d044e599b7aeb9b1f03b16ac9
chelseabhuiyan <bhuiyanfchelsea@gmail.com>
2025-05-14 11:09:37 -0400

    updated main.cpp

--- Vehicles/src/main/java/com/pluralsight/Main.java
+++ Vehicles/src/main/java/com/pluralsight/Main.java
@@ -2,27 +2,18 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
-        Moped slowRide = new Moped();
-        slowRide.setColor("Red");
-        slowRide.setFuelCapacity(5);
+        Moped newMoped = new Moped("red",1,0,5);
 
-        Car newCar = new Car();
-        newCar.setColor("Blue");
-        newCar.setNumberOfPassengers(4);
-        newCar.setFuelCapacity(40);
+        Car newCar = new Car("blue",4,100, 40);
 
-        SemiTruck newTruck = new SemiTruck();
-        newTruck.setColor("White");
-        newTruck.setCargoCapacity(10000);
-        newTruck.setFuelCapacity(150);
 
-        Hovercraft newHoverCraft = new Hovercraft();
-        newHoverCraft.setColor("Silver");
-        newHoverCraft.setNumberOfPassengers(6);
-        newHoverCraft.setFuelCapacity(80);
+        SemiTruck newTruck = new SemiTruck("white",4,1000,150);
+
+        Hovercraft newHoverCraft = new Hovercraft("Silver", 6, 10,80);
+
 
         // Output test
-        System.out.println("Moped color: " + slowRide.getColor());
+        System.out.println("Moped color: " + newMoped.getColor());
         System.out.println("Car passengers: " + newCar.getNumberOfPassengers());
         System.out.println("Truck cargo capacity: " + newTruck.getCargoCapacity());
         System.out.println("Hovercraft fuel capacity: " + newHoverCraft.getFuelCapacity());
